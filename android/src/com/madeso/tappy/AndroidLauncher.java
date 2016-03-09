948ef0d08b0eadb936bcee5ee5a33a9d9202c12f
Gustav <sir.gustav.the.coder@gmail.com>
2016-03-09 07:49:55 +0100

    initial project

---
+++ android/src/com/madeso/tappy/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.madeso.tappy;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.madeso.tappy.TappyPlane;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TappyPlane(), config);
+	}
+}
